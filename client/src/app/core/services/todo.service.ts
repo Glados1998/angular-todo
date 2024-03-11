4733c4e28e7b58fe6a3283af61de7a36267e853b
jerome greder <j.greder@staro.ch>
2024-03-11 11:28:49 +0100

    Update API endpoint and adjust interval in TodoService
    API endpoint in TodoService was updated to use 'api' instead of 'angular-todo-api' for better clarity. The interval for the watchTodo function was also increased from 1 second to 3 seconds to decrease server load.

--- client/src/app/core/services/todo.service.ts
+++ client/src/app/core/services/todo.service.ts
@@ -7,36 +7,36 @@ import {interval, switchMap} from "rxjs";
   providedIn: 'root'
 })
 export class TodoService {
-  private apiUrl = 'http://localhost:5292/angular-todo-api';
+  private apiUrl = 'http://localhost:5292/api';
 
   constructor(private http: HttpClient) {
   }
 
   watchTodo() {
-    return interval(1000).pipe(switchMap(() => this.getTodos()));
+    return interval(3000).pipe(switchMap(() => this.getTodos()));
   }
 
   getTodos() {
-    return this.http.get<Todo[]>(`${this.apiUrl}/todo`);
+    return this.http.get<Todo[]>(`${this.apiUrl}/Todo`);
   }
 
   getTodoById(id: number) {
-    return this.http.get<Todo>(`${this.apiUrl}/todo/${id}`);
+    return this.http.get<Todo>(`${this.apiUrl}/Todo/${id}`);
   }
 
   addTodo(data: Todo) {
-    return this.http.post<Todo>(`${this.apiUrl}/todo`, data);
+    return this.http.post<Todo>(`${this.apiUrl}/Todo`, data);
   }
 
   updateTodo(id: number, updatedData: Todo) {
-    return this.http.put<Todo>(`${this.apiUrl}/todo/${id}`, updatedData);
+    return this.http.put<Todo>(`${this.apiUrl}/Todo/${id}`, updatedData);
   }
 
   deleteTodo(id: number) {
-    return this.http.delete(`${this.apiUrl}/todo/${id}`);
+    return this.http.delete(`${this.apiUrl}/Todo/${id}`);
   }
 
   completeTodo(id: number, updatedData: Todo) {
-    return this.http.put<Todo>(`${this.apiUrl}/todo/${id}`, updatedData);
+    return this.http.put<Todo>(`${this.apiUrl}/Todo/${id}`, updatedData);
   }
 }
